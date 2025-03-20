ca75b05a570e3c0f344c296ee7d5c7c5b560bd1b
aparajit0511 <aparajit35@gmail.com>
2025-03-20 21:26:14 +0530

    more codes

--- JS_Interview_program.js
+++ JS_Interview_program.js
@@ -34,3 +34,30 @@ for (var i = 0; i < sentence.length; i++) {
 }
 
 console.log(reversedString);
+
+//-----------------------------------------------------------------------------
+
+// frequency of elements ans most repeated element
+var nums = [1, 1, 2, 3, 3, 3, 4, 5, 5, 8, 10];
+var freq = {};
+
+for (var i = 0; i < nums.length; i++) {
+  if (nums[i] in freq) {
+    freq[nums[i]] += 1;
+  } else {
+    freq[nums[i]] = 1;
+  }
+}
+
+console.log(freq);
+var max = 0;
+var ans = 0;
+
+for (var [key, value] of Object.entries(freq)) {
+  if (value > max) {
+    max = value;
+    ans = key;
+  }
+}
+
+console.log("Element " + ans + " repeats " + max + " times");
