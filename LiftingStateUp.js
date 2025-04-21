1eb4872c1257e90b0e8e1d401271c40128e6dc96
aparajit chatterjee <aparajit35@gmail.com>
2025-04-22 01:14:09 +0530

    Update LiftingStateUp.js

--- LiftingStateUp.js
+++ LiftingStateUp.js
@@ -1,3 +1,36 @@
+///// short example
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [color, setColor] = useState("red");
+
+  const onChangeColor = (color) => {
+    setColor(color);
+  };
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>Current color is: {color}</h3>
+      <Child onChangeColor={onChangeColor} />
+    </div>
+  );
+}
+
+function Child({ onChangeColor }) {
+  return (
+    <>
+      <button onClick={() => onChangeColor("blue")}>Blue</button>
+      <button onClick={() => onChangeColor("pink")}>Pink</button>
+      <button onClick={() => onChangeColor("red")}>Reset</button>
+    </>
+  );
+}
+
+
+
+
 // App.js
 import { useState } from "react";
 import Child from "./Child";
